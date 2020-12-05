841a9ee1aa0dbd9b740d970d87fbad2e54c6952c
Cheng® <cheng106.andy@gmail.com>
2020-12-05 17:09:33 +0800

    1.[add] 商品三級分類

--- yumall-product/src/main/java/com/cheng/yumall/product/entity/CategoryEntity.java
+++ yumall-product/src/main/java/com/cheng/yumall/product/entity/CategoryEntity.java
@@ -1,15 +1,18 @@
 package com.cheng.yumall.product.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
+
 import lombok.Data;
 
 /**
  * 商品三級分類
- * 
+ *
  * @author Cheng®
  * @email cheng106.andy@gmail.com
  * @date 2020-11-09 23:46:12
@@ -17,44 +20,47 @@ import lombok.Data;
 @Data
 @TableName("pms_category")
 public class CategoryEntity implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * 分類id
-	 */
-	@TableId
-	private Long catId;
-	/**
-	 * 分類名稱
-	 */
-	private String name;
-	/**
-	 * 父分類id
-	 */
-	private Long parentCid;
-	/**
-	 * 層級
-	 */
-	private Integer catLevel;
-	/**
-	 * 是否顯示[0-不顯示，1顯示]
-	 */
-	private Integer showStatus;
-	/**
-	 * 排序
-	 */
-	private Integer sort;
-	/**
-	 * icon
-	 */
-	private String icon;
-	/**
-	 * 計量單位
-	 */
-	private String productUnit;
-	/**
-	 * 商品數量
-	 */
-	private Integer productCount;
+    /**
+     * 分類id
+     */
+    @TableId
+    private Long catId;
+    /**
+     * 分類名稱
+     */
+    private String name;
+    /**
+     * 父分類id
+     */
+    private Long parentCid;
+    /**
+     * 層級
+     */
+    private Integer catLevel;
+    /**
+     * 是否顯示[0-不顯示，1顯示]
+     */
+    private Integer showStatus;
+    /**
+     * 排序
+     */
+    private Integer sort;
+    /**
+     * icon
+     */
+    private String icon;
+    /**
+     * 計量單位
+     */
+    private String productUnit;
+    /**
+     * 商品數量
+     */
+    private Integer productCount;
 
+    // 表示在資料庫中不存在該欄位
+    @TableField(exist = false)
+    private List<CategoryEntity> children;
 }
--- yumall-product/src/main/java/com/cheng/yumall/product/service/impl/CategoryServiceImpl.java
+++ yumall-product/src/main/java/com/cheng/yumall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,12 @@
 package com.cheng.yumall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -16,14 +21,55 @@ import com.cheng.yumall.product.service.CategoryService;
 @Service("categoryService")
 public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity> implements CategoryService {
 
+
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<CategoryEntity> page = this.page(
                 new Query<CategoryEntity>().getPage(params),
-                new QueryWrapper<CategoryEntity>()
+                new QueryWrapper<>()
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        // 查出所有分類
+        // MyBatis的baseMapper等於是categoryDao (參數傳入null等於查詢全部)
+        List<CategoryEntity> categoryEntityList = baseMapper.selectList(null);
+        // 組成父子的樹狀結構
+
+//        List<CategoryEntity> levelOneMenus = categoryEntityList.stream().filter(entity -> entity.getParentCid() == 0)
+//                .map(menu -> {
+//                    menu.setChildren(getChildren(menu, categoryEntityList));
+//                    return menu;
+//                }).sorted((menu1, menu2) -> {
+//                    return menu1.getSort() - menu2.getSort();
+//                }).collect(Collectors.toList());
+
+        // 找出全部的一級分類，一級分類的父分類為0
+        return categoryEntityList.stream().filter(entity -> entity.getParentCid() == 0)
+                .peek(menu -> menu.setChildren(getChildren(menu, categoryEntityList)))
+                .sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort())))
+                .collect(Collectors.toList());
+    }
+
+    private List<CategoryEntity> getChildren(CategoryEntity root, List<CategoryEntity> all) {
+//        all.stream().filter(categoryEntity -> {
+//            return categoryEntity.getParentCid().equals(root.getCatId());
+//        }).map(categoryEntity -> {
+        // 找到子選單
+//            categoryEntity.setChildren(getChildren(categoryEntity, all));
+//            return categoryEntity;
+//        }).sorted((menu1, menu2) -> {
+        // 選單的排序
+//            return menu1.getSort() - menu2.getSort();
+//        }).collect(Collectors.toList());
+
+        return all.stream().filter(categoryEntity -> categoryEntity.getParentCid().equals(root.getCatId()))
+                .peek(categoryEntity -> categoryEntity.setChildren(getChildren(categoryEntity, all)))
+                .sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort())))
+                .collect(Collectors.toList());
     }
 
 }
