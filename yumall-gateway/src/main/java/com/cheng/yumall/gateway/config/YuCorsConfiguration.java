ee9e2cb149649a20b7427e60f7bb134958fa093f
Cheng® <cheng106.andy@gmail.com>
2020-12-06 19:58:05 +0800

    1.[add] CORS config

---
+++ yumall-gateway/src/main/java/com/cheng/yumall/gateway/config/YuCorsConfiguration.java
@@ -0,0 +1,32 @@
+package com.cheng.yumall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * @ClassName CorsConfiguration
+ * @Description 解決登入時發生CORS的問題
+ * @Author cheng
+ * @Date 2020/12/6 19:03
+ * @Version 1.0
+ **/
+@Configuration
+public class YuCorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+}
